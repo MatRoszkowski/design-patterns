fcb3d9d25ea3237735e4e98983a1540ffe98b0e0
Mateusz Roszkowski <mateusz.roszkowski96@gmail.com>
2022-05-14 11:36:20 +0200

    hole - peg adapter example

--- src/main/java/com/mateuszroszkowski/design_patterns/adapter/Main.java
+++ src/main/java/com/mateuszroszkowski/design_patterns/adapter/Main.java
@@ -1,10 +1,15 @@
 package com.mateuszroszkowski.design_patterns.adapter;
 
 import com.mateuszroszkowski.design_patterns.adapter.duckTurkeyExample.*;
+import com.mateuszroszkowski.design_patterns.adapter.holePegExample.RoundHole;
+import com.mateuszroszkowski.design_patterns.adapter.holePegExample.RoundPeg;
+import com.mateuszroszkowski.design_patterns.adapter.holePegExample.SquarePeg;
+import com.mateuszroszkowski.design_patterns.adapter.holePegExample.SquarePegAdapter;
 
 public class Main {
     public static void main(String[] args) {
-        duckTurkeyExample();
+//        duckTurkeyExample();
+        holePegExample();
     }
 
     private static void duckTurkeyExample(){
@@ -24,5 +29,29 @@ public class Main {
         System.out.println("Turkey adopted to duck: ");
         turkeyAdapter.quack();
         turkeyAdapter.fly();
+    }
+
+    private static void holePegExample(){
+        // Round fits round, no surprise.
+        RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        // hole.fits(smallSqPeg); // Won't compile.
+
+        // Adapter solves the problem.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+
     }
 }
---
+++ src/main/java/com/mateuszroszkowski/design_patterns/adapter/holePegExample/RoundPeg.java
@@ -0,0 +1,19 @@
+package com.mateuszroszkowski.design_patterns.adapter.holePegExample;
+
+/**
+ * RoundPegs are compatible with RoundHoles.
+ */
+public class RoundPeg {
+    private double radius;
+
+    public RoundPeg() {
+    }
+
+    public RoundPeg(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
